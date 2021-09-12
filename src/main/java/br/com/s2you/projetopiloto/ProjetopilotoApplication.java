78435c292ca4adb658d09dce56271229bd3112fc
Sidnei Coimbra <sidneicoimbra@yahoo.com.br>
2021-09-12 16:13:28 -0300

    Projeto Criado

---
+++ src/main/java/br/com/s2you/projetopiloto/ProjetopilotoApplication.java
@@ -0,0 +1,13 @@
+package br.com.s2you.projetopiloto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopilotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopilotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/s2you/projetopiloto/ProjetopilotoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.s2you.projetopiloto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopilotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
